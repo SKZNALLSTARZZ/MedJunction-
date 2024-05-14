304f7e93f38ae25d0eccbfcc9017ff34aaf18428
SKZNALLSTARZZ <skznallstarzz@gmail.com>
2024-05-14 11:26:50 +0100

    TwilioService + PassgenService + FileuploadRepo + UploadsFolder

--- app/Services/TwilioService.php
+++ app/Services/TwilioService.php
@@ -7,9 +7,7 @@ class TwilioService
 {
     public static function sendMessage($recipientPhone, $message)
     {
-        $twilioAccountSid = 'AC53c3ad7483fdcad468ac8ccee7748015';
-        $twilioAuthToken = '76694f48d1eb3ee9007c9712777a8648';
-        $twilioWhatsAppNumber = '+14155238886';
+
 
         $client = new Client($twilioAccountSid, $twilioAuthToken);
 
