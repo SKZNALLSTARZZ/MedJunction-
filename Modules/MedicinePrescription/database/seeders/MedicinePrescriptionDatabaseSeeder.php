05d1b7e573cd6836ac0a65890d82cd76c945b829
yassine fahchouch <fahchouch27@gmail.com>
2024-05-30 11:59:26 +0200

    add MedicinePrescription Pivot Model

---
+++ Modules/MedicinePrescription/database/seeders/MedicinePrescriptionDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\MedicinePrescription\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class MedicinePrescriptionDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
